e112ceb1d71c9226b8c5e8b7e880188c5dd616a7
Anuj Patel <patelanuj28@gmail.com>
2014-08-18 00:09:45 -0700

    Fixed some errors

--- convert_name.py
+++ convert_name.py
@@ -3,9 +3,7 @@
 import os, sys, glob, getopt
 import os.path
 import re
-
-print os.path.splitext("path_to_file")[0]
-
+current_file_name = os.path.basename(__file__)
 
 class clean_songs_name():
 
@@ -15,11 +13,10 @@ class clean_songs_name():
 	def __init__(self):
 		self.index = 0
 		#self.clean_name(path)
-
 	
-	def clean_file_name(self, dirpath, newdirname, filename):
+	def clean_file_name(self, dirpath, filename):
 		p = re.I
-		p = re.compile('(_)|(2014)|(2013)|(2012)|(2010)|(2011)|(2009)|(Songs)|(128)|(320)|(Kbps)|(.PK)|(\()|(\)|(-))|(\[)|(\])|(www.?)|(www)|('+newdirname+')')
+		p = re.compile('(_)|(2014)|(2013)|(2012)|(2010)|(2011)|(2009)|(Songs)|(128)|(320)|(Kbps)|(.PK)|(\()|(\)|(-))|(\[)|(\])|(www.?)|(www)')
 		if os.path.basename(filename).endswith('.mp3'):
 			newfilename = p.sub ('', os.path.basename(filename))
 			p = re.compile('(\s+)')
@@ -32,12 +29,19 @@ class clean_songs_name():
 				with open(newfilename): pass
 			except IOError:
 				if newfilename == "":
-					newfilename = newdirname + index
+					newfilename = filename + self.index
 					self.index += 1
-                os.rename(filename, dirname+"/"+newfilename)
-                return newfilename					
+			os.rename(filename, dirname+"/"+newfilename)
+			print newfilename
 
-	def clean_directory_name(self, dirpath, dirname):
+	def clean_directory_name(self, dirpath):
+		
+		path = os.path.dirname(dirpath)
+		dirname = os.path.basename(dirpath)
+		
+		if not path or not dirname:
+			return
+		
 		p = re.I
 		p = re.compile('(_)|(2014)|(2013)|(2012)|(2010)|(2011)|(2009)|(Songs)|(128)|(320)|(Kbps)|(WWW.?)|(.PK)|(\()|(\)|(-))')
 		newdirname = p.sub (' ', dirname)
@@ -45,17 +49,17 @@ class clean_songs_name():
 		newdirname = p.sub (' ', newdirname)
 		newdirname = newdirname.strip()
 
+		newdirname = "%s/%s" % (path, newdirname)
+
 		if os.path.isdir(newdirname):
 			pass
 		else:
+
 			if newdirname == "":
-				newdirname = dirname
-
+				newdirname = dirname			
 			if self.rename_current_directory:
-				os.rename(os.path.join(dirpath, dirname), os.path.join(dirpath, newdirname))
+				os.rename(dirpath, newdirname)
 			
-			return newdirname
-
 	def clean_name(self, path):
 		'''
 		@TODO - allow user to pass directory path from command line argument. 
@@ -63,62 +67,55 @@ class clean_songs_name():
 		'''
 		self.path = path
 		mypath = self.path
+		
 		if not os.path.isdir(mypath):
 			print 'Error : Path \'' + mypath + '\' does not exists. '
 			sys.exit(1)
 
 		try:
 
+			for (dirpath, dirnames, filenames) in os.walk(mypath,topdown=False):				
+				#if len(dirnames) > 0:
+				if dirpath:
+					newdirname = self.clean_directory_name(dirpath)													
+
 			for (dirpath, dirnames, filenames) in os.walk(mypath):
-				if len(dirnames) > 0:
-
-					for idx in range(len(dirnames)):
-						newdirname = self.clean_directory_name(dirpath, dirnames[idx])
-						list_of_mp3 = glob.glob(dirpath+"/"+newdirname+"/*.mp3")
-						
-						for filename in list_of_mp3:
-							newfilename = self.clean_file_name(dirpath, newdirname, filename)
-				else:
-					newdirname = os.path.basename(dirpath)
-					
-					newdirname = self.clean_directory_name(dirpath, os.path.basename(dirpath))
-
-
-					list_of_mp3 = glob.glob(dirpath+"/*.mp3")
-
-					self.index = 0
-					for filename in list_of_mp3:
-						newfilename = self.clean_file_name(dirpath, newdirname, filename)
-						print newfilename
-
+				for f in filenames:
+					if f.endswith(".mp3"):
+						self.index = 0
+						fname = "%s/%s" % (dirpath, f)
+						newfilename = self.clean_file_name(dirpath, fname)										
 
 		except OSError as e:
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-			print(exc_type, fname, exc_tb.tb_lineno)
-
+			print(exc_type, fname, exc_tb.tb_lineno, str(e))
+		except Exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			print(exc_type, fname, exc_tb.tb_lineno, str(e))
 
 def main(argv):
 	try:
-		opts, args = getopt.getopt(argv,"hvp:v:c:",["p=","path=","version", "rename-current=", "c="])
+		opts, args = getopt.getopt(argv,"hvp:v:c:",["p=","path=","version", "rename-dir=", "c="])
 	except getopt.GetoptError:
-		print 'clean_songs_name.py -p <directory path> -v <version>'
+		print '%s -p <directory path> -v <version>' % current_file_name
 	 	sys.exit(2)
 
 	print ""
 	try:
 		change_dirname = clean_songs_name()
-		change_dirname.rename_current_directory = False
+		
 		for opt, arg in opts:
 			if opt == '-h':
-				print 'clean_songs_name.py -p <directory path> -v <version>'
+				print '%s -p <directory path> -v <version>' % current_file_name
 				sys.exit()
 			elif opt in ('-v', '--version'):
 				print "Copyright by 	: Anuj Patel"
-				print 'Version		: v1.0'
+				print 'Version		: v1.1'
 				sys.exit()
-			elif opt in ('--rename-current', '-c'):
-				if arg == True:
+			elif opt in ('--rename-dir', '-c'):
+				if arg.lower() == "true":
 					change_dirname.rename_current_directory = True
 				#sys.exit()
 			elif opt in ("-p", "--path"):
@@ -126,16 +123,13 @@ def main(argv):
 				x = change_dirname.clean_name(str(arg))
 				sys.exit()
 			else:
-				print 'clean_songs_name.py -p <directory path> -v <version>'
+				print '%s -p <directory path> -v <version>' % current_file_name
 				sys.exit()
 
 	except Exception as e:
 		exc_type, exc_obj, exc_tb = sys.exc_info()
 		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-		print(exc_type, fname, exc_tb.tb_lineno)
+		print(exc_type, fname, exc_tb.tb_lineno, str(e))
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
-
-
-
